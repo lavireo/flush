9dfc4ee626948f03c005a220eeefa1d5ddb0723f
Maurice T. Meyer <maurice@lavireo.com>
2016-10-13 12:25:49 +0200

    Initial commit

---
+++ bin/flush.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+/*
+ *
+ * flush.js
+ *
+ * Author: Maurice T. Meyer
+ * E-Mail: maurice@lavireo.com
+ *
+ * Date: 10-10-2016
+ *
+ * (c) Laviréo 2016
+ */
+
+const fs   = require('fs-plus');
+const path = require('path');
